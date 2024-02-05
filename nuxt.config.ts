ce4db09f742cbf7c2f5db25836e7b7e4ddf65cea
André Luis dos Santos <alsss.ads@gmail.com>
2024-02-05 11:48:16 -0300

    chore: :tada: Inicia o projeto

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/supabase',
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt',
+  ], runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BURCKET_URL,
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>
+    Welcome to my threads clone!
+  </h1>
+</template>
